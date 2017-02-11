5ca843337941359129835ad9d52c6b1d0ef93d1c
CaioRS <caiorss.rodrigues@gmail.com>
2017-02-11 00:38:02 -0300

    Fix docker-entrypoint.sh.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+export PGDATA=/data 
+export FILE_MARK=$PGDATA/fileflag
+
 # Set the permission of postgres directory 
 chown -R postgres:postgres $PGDATA 
-
-FILE_MARK=$PGDATA/fileflag
 
 # Create initial database
 gosu postgres initdb
 
+sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
+
 if [ ! -f $FILE_MARK ]
-then echo "host  all   all   0.0.0.0/0   trust" >> /data/pg_hba.conf
-else touch $FILE_MARK
+then
+     echo "host  all   all   0.0.0.0/0   trust" >> /data/pg_hba.conf
+     touch $FILE_MARK
 fi
 
 gosu postgres postgres -D /data -c config_file=/data/postgresql.conf
